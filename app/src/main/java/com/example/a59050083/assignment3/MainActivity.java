c77bd9329eb4256e7468f9f9a96ed6e04914718f
prakasit <prakasitbm@gmail.com>
2018-09-08 16:35:03 +0700

    class anonymous//getId is the best //bad instanOf couldn't used

--- app/src/main/java/com/example/a59050083/assignment3/MainActivity.java
+++ app/src/main/java/com/example/a59050083/assignment3/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     //step 1: declare
     private TextView tv1;
     private EditText et1;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView iv1;
     private  ImageView iv2;
     private Button bt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,22 +34,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt1 = findViewById(R.id.button);
         tv1 = findViewById(R.id.textView);
 
-        bt1.setOnClickListener(this);
-        tv1.setOnClickListener(this);
-        et2.setOnClickListener(this);
-        et1.setOnClickListener(this);
+        bt1.setOnClickListener(new View.OnClickListener() { //click and show toast
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Click", Toast.LENGTH_LONG).show();
 
+                String a = et1.getText().toString();
+                tv1.setText(a);
+                tv1.setTextColor(Color.parseColor("#00ff00"));
+                iv1.setImageResource(R.drawable.mpl269);
+            }
+        });
+
+        bt1.setOnClickListener(this);
+
+        et1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tv1.setText(s.toString());
+            }
+        });
     }
+
     @Override
     public void onClick(View v) {
-        if(v instanceof  TextView && v.getId() == R.id.button) { // <--chk type view , (v.getId() == R.id.button) chk id layer
-            Toast.makeText(this,"this is button",Toast.LENGTH_LONG).show();
-        } else if(v.getId() == R.id.textView) {
-            Toast.makeText(this,"this is textView",Toast.LENGTH_LONG).show();
-        } else if(v.getId() == R.id.editText2) {
-            Toast.makeText(this,"this is editText2",Toast.LENGTH_LONG).show();
-        } else if(v instanceof EditText) {
-            Toast.makeText(this,"this is editText1",Toast.LENGTH_LONG).show();
+        if(v instanceof  Button) {
+            Toast.makeText(MainActivity.this,"this is button",Toast.LENGTH_LONG).show();
+        } else if(v instanceof TextView) {
+            Toast.makeText(this,"this is textiview",Toast.LENGTH_LONG).show();
         }
     }
 }
